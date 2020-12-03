bb111920873071e44d8144f9dc5e0a3a79d617e8
Stian Støer Ødegård <stian.odegard@outlook.com>
2020-12-03 20:21:42 +0100

    not quite there

--- dayThree.py
+++ dayThree.py
@@ -3,6 +3,8 @@
 # move 3 right, 1 down, continue
 # Count trees you hit in the way
 
+import re
+
 def splitString(line):
     line = line.rstrip("\n")
     return [char for char in line]
@@ -12,40 +14,40 @@ def findTree(character):
         return 1
     else:
         return 0
-
-def getSlope()
-    with open("./resource/slopes.txt", "r") as f:
-        for line in f:
-            sideway = line[0]
-            down = line [1]
-
-
+    
 slopes = []
-
 rightMove = 1
 sideway = 0
 down = 0
 trees = 0
 l = 1
 
-with open("./resource/daythreeslope.txt","r") as f:
-    for line in f:
-        x = splitString(line)
-        chars = len(x)
+with open("./resource/daythreeslope.txt","r") as slope:
+    with open("./resource/slopes.txt", "r") as paths:
+        for path in paths:
+            path = re.match("([0-9]+),([0-9]+)", path).groups()
+            sideway = int(path[0])
+            down = int(path[1])
+            print(sideway,down)
+            for line in slope:
+                x = splitString(line)
+                chars = len(x)
 
-        if l != 1:
-            rightMove += 3
+                if l != 1:
+                    rightMove += sideway
 
-        if rightMove > chars:
-            rightMove = rightMove % chars
+                if rightMove > chars:
+                    rightMove = rightMove % chars
 
-            #Only for debugging
-            print("index: ", rightMove, " ", x[rightMove])
+                    #Only for debugging
+                    #print("index: ", rightMove, " ", x[rightMove])
 
-        trees += findTree(x[rightMove-1])
-        print("line:", l, "index:", rightMove,"trees", trees, "symbol:", x[rightMove-1])
-        
-        #Only for debugging
-        l += 1
+                trees += findTree(x[rightMove-1])
+                #print("line:", l, "index:", rightMove,"trees", trees, "symbol:", x[rightMove-1])
+
+                #Only for debugging
+                l += 1
+        print(trees)
+                
 
         
